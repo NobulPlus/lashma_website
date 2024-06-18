86cd0ce711241e8ea89aa5e08332604719e02247
NobulPlus <chenko4me@gmail.com>
2024-06-19 00:04:57 +0100

    GA integration

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize("G-16Y7GXWY2D");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
